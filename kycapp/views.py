25ed4c9592c8cac2a99cc4e4ed1cb6c2637ff8b5
Kanyango <kariukikanyango@gmail.com>
2019-04-09 13:56:41 +0300

    KYC APP

---
+++ kycapp/views.py
@@ -0,0 +1,25 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from passporteye import read_mrz
+from rest_framework import parsers
+from rest_framework import status
+
+class FileUploadView(APIView):
+    # parser_classes = (parsers.MultiPartParser, )
+
+    def post(self, request):
+        up_file = 'https://firebasestorage.googleapis.com/v0/b/kycapp-18ab0.appspot.com/o/id2.jpg?alt=media&token=623a27e4-d508-4024-b360-bc2126c5120d'
+        # up_file = request.FILES['myfile']
+        print('Faili', up_file)
+        mrz = read_mrz(up_file)
+        pass_data = mrz.to_dict()
+        print("Mrz fILE",pass_data["optional2"])
+        # destination = open('/Users/Username/' + up_file.name, 'wb+')
+        # for chunk in up_file.chunks():
+        #     destination.write(chunk)
+        #     destination.close()
+        # ...
+        # do some stuff with uploaded file
+        # ...
+        # return Response(mrz, status.HTTP_201_CREATED)
