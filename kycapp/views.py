84379592fec0f76ffc45c52e634c83b34a12d292
Kanyango <kariukikanyango@gmail.com>
2019-04-16 22:12:56 +0300

    Update views.py

--- kycapp/views.py
+++ kycapp/views.py
@@ -1,23 +1,23 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response, JsonResponse
+from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from passporteye import read_mrz
+from django.http import HttpResponse, JsonResponse
 from rest_framework import parsers
 from rest_framework import status
+import json
+
 
 class FileUploadView(APIView):
     # parser_classes = (parsers.MultiPartParser, )
 
     def post(self, request):
-        img_url = request.data["img_url"]
-        print('Image url', img_url)
-        up_file = 'https://firebasestorage.googleapis.com/v0/b/kycapp-18ab0.appspot.com/o/id2.jpg?alt=media&token=623a27e4-d508-4024-b360-bc2126c5120d'
-        # up_file = request.FILES['myfile']
+        #up_file = 'https://firebasestorage.googleapis.com/v0/b/kycapp-18ab0.appspot.com/o/dfc195e4-9eed-44de-973f-4a2b03d8ec4f1825852140.jpg?alt=media&token=98e7f9f3-84c8-41a1-a277-ad55375121f6'
+        up_file = request.data['myfile']
         print('Faili', up_file)
         mrz = read_mrz(up_file)
         pass_data = mrz.to_dict()
         print("Mrz fILE",pass_data["optional2"])
-        
         # destination = open('/Users/Username/' + up_file.name, 'wb+')
         # for chunk in up_file.chunks():
         #     destination.write(chunk)
@@ -25,4 +25,5 @@ class FileUploadView(APIView):
         # ...
         # do some stuff with uploaded file
         # ...
-       return JsonResponse(mrz, status.HTTP_201_CREATED)
+        json_data = json.dumps(pass_data)
+        return JsonResponse(json_data, safe=False, status=200)
